2c13b650c6aefe9af12aae0387256755498fae2f
Will Dickinson <willdickinson98@gmail.com>
2021-01-11 21:56:36 -0700

    much improvement. many wow. now uses vue cdn and the code looks much nicer

--- script.js
+++ script.js
@@ -1,262 +1,155 @@
-const COOL_LETTERS_TIMER = 150
+let app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            helpModal: false,
+            towers: [[], [], []],
+            disks: [],
+            numDisks: 3,
+            maxDisks: 10,
+            moveNumber: 0,
+            solving: false,
+            doneSolving: false,
+            solveSpeed: 700,
+            moveQueue: []
+        };
+    },
 
-var diskSlider = document.querySelector("#diskSlider")
-var numDisks = document.querySelector("#numDisks")
-var minMoves = document.querySelector("#minMoves")
-var moveDisplay = document.querySelector("#moveNumber").children[0]
+    created() {
+        this.initializeDisks();
+        this.reset();
+        setInterval(() => {
+            if (this.perfect)
+                this.coolLetters(this.$refs['perfect']);
+        }, 200);
+        // For closing the modal
+        window.addEventListener("click", (e) => {
+            if (e.target == this.$refs['helpModal']) {
+                this.helpModal = false;
+            }
+        });
+    },
 
-var solveButton = document.querySelector("#solve")
-var resetButton = document.querySelector("#reset")
-var speedSlider = document.querySelector("#solveSpeed")
+    methods: {
+        reset() {
+            this.solving = false;
+            this.moveQueue = [];
+            this.moveNumber = 0;
+            this.towers = [[], [], []];
+            for (const disk of this.curDisks) {
+                this.towers[0].push(disk.num);
+            }
+        },
+        coolLetters(el) {
+            // This is pretty much the only legacy function as it queries the DOM directly
+            for (const letter of el.children) {
+                let r = Math.random() * 256;
+                let g = Math.random() * 256;
+                let b = Math.random() * 256;
+                let size = (Math.random() * 5) + 30;
 
-// Completion displays
-var perfectWin = document.querySelector("#perfectWin")
-var perfectTimer
-var win = document.querySelector("#win")
-var solved = document.querySelector("#solved")
-
-var tower1 = document.querySelector("#one")
-var tower2 = document.querySelector("#two")
-var tower3 = document.querySelector("#three")
-
-// Local data
-var towers = [tower1, tower2, tower3]
-var disks = []
-var moveQueue = []
-var moveCount = 0
-var solving = false
-
-diskSlider.oninput = reset;
-
-perfectTimer = setInterval(() => {
-    coolLetters(perfectWin)
-}, COOL_LETTERS_TIMER);
-
-solveButton.onclick = () => {
-    solveButton.disabled = true
-    solving = true
-    solve(disks.length, 1, 3, 2)
-    beginSolve(doMove)
-}
-
-function doMove() {
-    if (moveQueue.length != 0) {
-        let move = moveQueue.shift()
-        move()
-    }
-
-    isDone(false)
-}
-
-function beginSolve(callback) {
-    var nextMove = function () {
-        callback()
-        if (solving) {
-            window.setTimeout(nextMove, speedSlider.value)
-        }
-    }
-    nextMove()
-};
-
-resetButton.onclick = reset
-
-randomizeDiskClasses()
-changeDisks()
-
-function reset() {
-    moveQueue = []
-    solving = false
-    changeDisks()
-}
-
-function changeDisks() {
-    let n = diskSlider.value
-    numDisks.innerHTML = n
-    minMoves.innerHTML = `${(2 ** n) - 1} (2<sup>${n}</sup>-1) moves`
-
-    // change HTML
-    resetDisks(n)
-}
-
-function resetDisks(n) {
-    tower1.innerHTML = ""
-    tower2.innerHTML = ""
-    tower3.innerHTML = ""
-    disks = []
-
-    for (let index = n; index > 0; index--) {
-        let newDisk = document.createElement("div")
-        newDisk.classList.add("disk")
-        newDisk.id = `disk-${index}`
-        newDisk.dataset.number = index
-
-        disks.push(newDisk)
-        tower1.appendChild(newDisk)
-    }
-
-    win.style.display = "none"
-    solved.style.display = "none"
-    perfectWin.style.display = "none"
-
-    updateDisks()
-}
-
-function makeTop(disk) {
-    disk.draggable = true
-    disk.ondragstart = (e) => { drag(e) }
-    disk.classList.add("topDisk")
-}
-
-function updateDisks() {
-    // Make all disks unusable
-    disks.forEach(disk => {
-        disk.classList.remove("topDisk")
-        disk.draggable = false
-        disk.removeEventListener("ondragstart", (event) => { drag(event) })
-    });
-
-    // Check for win
-    if (tower3.children.length == disks.length) {
-        win.style.display = "block"
-    }
-
-    // Make top disk usable
-    for (let index = 0; index < towers.length; index++) {
-        let tower = towers[index]
-        let top = getTopDisk(tower)
-        if (top != -1) {
-            makeTop(top)
-        }
-    }
-
-    moveDisplay.innerHTML = moveCount
-}
-
-/**
- * Returns true if game is successfully completed
- * @param {boolean} user Did the user solve it?
- */
-function isDone(user) {
-    if (tower3.children.length == disks.length) {
-        if (user) {
-            // Was it done in the minimum number of moves?
-            if (moveCount == (2 ** numDisks) - 1) {
-                perfectWin.style.display = "block"
+                letter.style = `color: rgb(${r}, ${g}, ${b}); font-size: ${size}px;`;
+            }
+        },
+        solve(N, source, destination, other) {
+            if (N == 1) {
+                this.moveQueue.push(() => { this.moveDisk(source, destination); });
             }
             else {
-                win.style.display = "block"
+                this.solve(N - 1, source, other, destination);
+                this.solve(1, source, destination, other);
+                this.solve(N - 1, other, destination, source);
+            }
+        },
+        moveDisk(source, destination) {
+            let src = this.towers[source];
+            let dst = this.towers[destination];
+            let disk = src[0];
+            src.splice(src.indexOf(disk), 1);
+            dst.unshift(disk);
+            this.moveNumber++;
+        },
+        beginSolve() {
+            this.reset();
+            this.solving = true;
+            this.solve(this.numDisks, 0, 2, 1);
+            this.beginSolveMoves(this.doMove);
+        },
+        beginSolveMoves(callback) {
+            let nextMove = () => {
+                callback();
+                if (!this.doneSolving) {
+                    window.setTimeout(nextMove, 1300 - this.solveSpeed);
+                }
+            };
+            nextMove();
+        },
+        doMove() {
+            if (this.moveQueue.length != 0) {
+                let move = this.moveQueue.shift();
+                move();
+            }
+            this.doneSolving = this.done;
+        },
+        allowDrop(e) {
+            e.preventDefault();
+        },
+
+        dragStart(e, tower, disk) {
+            e.dataTransfer.setData("tower", tower);
+            e.dataTransfer.setData("disk", disk);
+        },
+
+        drop(e, towerTo) {
+            e.preventDefault();
+            let tower = this.towers[towerTo];
+            let towerFrom = parseInt(e.dataTransfer.getData("tower"));
+            let disk = parseInt(e.dataTransfer.getData("disk"));
+
+            // If this disk is smaller than the top disk then continue
+            if (tower.length == 0 || disk < tower[tower.length - 1]) {
+                this.moveDisk(towerFrom, towerTo);
+            }
+            // Otherwise do nothing
+        },
+        randomizeDiskColors() {
+            for (const disk of this.disks) {
+                let r = Math.random() * 256;
+                let g = Math.random() * 256;
+                let b = Math.random() * 256;
+                this.$set(disk, 'background-color', `rgb(${r}, ${g}, ${b})`);
+            }
+        },
+        initializeDisks() {
+            for (let index = 0; index < this.maxDisks; index++) {
+                this.disks.push({ num: index + 1, width: `${50 + (index * 25)}px`, right: `${(46 + (index * 25)) / 2}px` });
+            }
+            this.randomizeDiskColors();
+        },
+    },
+    computed: {
+        curDisks() {
+            return this.disks.slice(0, this.numDisks);
+        },
+        done() {
+            return this.towers[2].length == this.numDisks;
+        },
+        minMoves() {
+            return 2 ** this.numDisks - 1;
+        },
+        perfect() {
+            return this.done && this.moveNumber == this.minMoves && !this.doneSolving;
+        },
+        movesPerSec() {
+            let n = 1000 / (1300 - this.solveSpeed);
+            return Math.round((n + Number.EPSILON) * 100) / 100;
+        }
+    },
+    watch: {
+        done(val) {
+            if (val) {
+                this.solving = false;
             }
         }
-        else {
-            solved.style.display = "block"
-            solving = false
-            solveButton.disabled = false
-        }
-        return true
     }
-    return false
-}
-
-function getTopDisk(tower) {
-    if (tower.children.length) {
-        return tower.children[tower.children.length - 1]
-    }
-    return -1
-}
-
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("id", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("id");
-    var disk = document.getElementById(data)
-    var tower
-
-    if (ev.target.classList.contains("disk")) {
-        tower = ev.target.parentElement
-    }
-    else if (ev.target.classList.contains("tower")) {
-        tower = ev.target
-    }
-    else if (ev.target.classList.contains("tower-area")) {
-        tower = ev.target.children[1]
-    }
-    else if (ev.target.classList.contains("tower-stand")) {
-        tower = ev.target.parentElement.children[1]
-    }
-
-    var top = getTopDisk(tower)
-    var topSize = 11
-    // If tower is not empty
-    if (top != -1) {
-        topSize = top.dataset.number
-    }
-
-    // If this disk is smaller than the top disk then continue
-    if (disk.dataset.number < topSize) {
-        tower.appendChild(disk);
-        moveCount++
-        updateDisks()
-    }
-    // Otherwise do nothing
-}
-
-function solve(N, source, destination, other) {
-    if (N == 1) {
-        moveQueue.push(() => { moveDisk(source, destination) })
-    }
-    else {
-        solve(N - 1, source, other, destination)
-        solve(1, source, destination, other)
-        solve(N - 1, other, destination, source)
-    }
-}
-
-function moveDisk(source, destination) {
-    let src = towers[source - 1]
-    let dst = towers[destination - 1]
-
-    let disk = getTopDisk(src)
-
-    dst.appendChild(disk)
-}
-
-function randomizeDiskClasses() {
-    var style = document.createElement('style');
-    style.type = 'text/css';
-
-    for (let index = 0; index < 10; index++) {
-        var r = Math.random() * 256
-        var g = Math.random() * 256
-        var b = Math.random() * 256
-
-        var str = `#disk-${index + 1} { 
-            background-color: rgb(${r}, ${g}, ${b}); 
-            width: ${50 + (index * 25)}px; 
-            right: ${(46 + (index * 25)) / 2}px;
-        }\n`
-
-        style.innerHTML += str;
-    }
-
-    document.getElementsByTagName('head')[0].appendChild(style);
-}
-
-function coolLetters(el) {
-    for (let index = 0; index < el.children.length; index++) {
-        var letter = el.children[index];
-
-        var r = Math.random() * 256
-        var g = Math.random() * 256
-        var b = Math.random() * 256
-        var size = (Math.random() * 5) + 30
-
-        letter.style = `color: rgb(${r}, ${g}, ${b}); font-size: ${size}px;`
-    }
-}
+});
